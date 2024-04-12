a4af188fd801b01a279cb315ac016e5090270fa8
RuixiangJiang <2037358823@qq.com>
2024-04-12 22:42:07 +0800

    update top bar

--- FrontEnd/src/layout/dashboard/TopNavbar.vue
+++ FrontEnd/src/layout/dashboard/TopNavbar.vue
@@ -126,10 +126,10 @@
                 <p class="d-lg-none">Log out</p>
               </a>
               <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item">Profile</a>
+                <a href="#/profile" class="nav-item dropdown-item">Profile</a>
               </li>
               <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item">Settings</a>
+                <a href="#/profile" class="nav-item dropdown-item">Settings</a>
               </li>
               <div class="dropdown-divider"></div>
               <li class="nav-link">
