4d3ad659bbec11801ed9d7fb4807a36d0a97d762
RuixiangJiang <2037358823@qq.com>
2024-04-14 21:10:53 +0800

    update event.vue with topbar

--- FrontEnd/src/pages/Event/Event.vue
+++ FrontEnd/src/pages/Event/Event.vue
@@ -1,65 +1,65 @@
 <template>
   <div>
-    <top-navbar></top-navbar>
-      <div class="content">
-        <fade-transition :duration="100" mode="out-in">
-          <div class="col-lg-4" :class="{ 'text-right': false }">
-            <card type="nav-tabs" class="text-left" style="width: 60rem;">
-              <div v-if="error" class="error">{{ error }}</div>
-              <div v-else>
-                <div slot="header" class="card-header-primary">
-                  EventID: {{ getEventID() }}
-                </div>
-                <div class="d-flex">
-                  <div class="col">
+    <top-navbar class="fixed-top"></top-navbar>
+    <div class="content"style="margin-top: 70px;">
+      <fade-transition :duration="100" mode="out-in">
+        <div class="col-lg-4" :class="{ 'text-right': false }">
+          <card type="nav-tabs" class="text-left" style="width: 60rem;">
+            <div v-if="error" class="error">{{ error }}</div>
+            <div v-else>
+              <div slot="header" class="card-header-primary">
+                EventID: {{ getEventID() }}
+              </div>
+              <div class="d-flex">
+                <div class="col">
+                  <div>
+                    <h2 class="card-title">Theme: {{ event.title }}</h2>
                     <div>
-                      <h2 class="card-title">Theme: {{ event.title }}</h2>
-                      <div>
-                        <base-button class="animation-on-hover" type="warning">Classification: {{ event.classification }}</base-button>
-                        <base-button class="animation-on-hover" type="default">Time: {{ event.time }}</base-button>
-                      </div>
-                      <card type="nav-tabs">
-                        <div slot="header" class="card-header-success">
-                          Content
-                        </div>
-                        <blockquote class="blockquote mb-0">
-                          <p>{{ event.content }}</p>
-                          <footer class="blockquote-footer">Hold by <cite title="Source Title">{{ event.holder }}</cite></footer>
-                        </blockquote>
-                      </card>
+                      <base-button class="animation-on-hover" type="warning">Classification: {{ event.classification }}</base-button>
+                      <base-button class="animation-on-hover" type="default">Time: {{ event.time }}</base-button>
                     </div>
-                  </div>
-                  <div class="col">
-                    <card class="card" style="width: 20rem;">
-                      <img slot="image" class="card-img-top" :src="`events/${getEventID()}/1.jpg`" alt="Card image cap">
-                      <p class="card-text">Picture 1</p>
+                    <card type="nav-tabs">
+                      <div slot="header" class="card-header-success">
+                        Content
+                      </div>
+                      <blockquote class="blockquote mb-0">
+                        <p>{{ event.content }}</p>
+                        <footer class="blockquote-footer">Hold by <cite title="Source Title">{{ event.holder }}</cite></footer>
+                      </blockquote>
                     </card>
                   </div>
                 </div>
-                <div class="d-flex justify-content-between align-items-center">
-                    <base-button class="animation-on-hover" type="primary">Reserve</base-button>
-                    <base-button class="animation-on-hover" type="danger">Cancel</base-button>
-                    <base-button class="animation-on-hover" type="success">Comment</base-button>
-                </div>
-                <div class="dropdown-divider"></div>
-                <div slot="footer" class="text-muted">
-                  SUSTech
-                </div>
-              </div>
-              <card>
-                <base-input class="has-success" value="Comment here..."></base-input>
-              </card>
-              <div>
-                <div v-for="(comment, cIndex) in event.comments" :key="cIndex">
-                  <card class="mb-3">
-                    <h4 class="card-title">{{ comment.username }}</h4>
-                    <p class="card-text">{{ comment.comment }}</p>
-                    <p class="card-text"><small class="text-muted">{{ comment.time }}</small></p>
+                <div class="col">
+                  <card class="card" style="width: 20rem;">
+                    <img slot="image" class="card-img-top" :src="`events/${getEventID()}/1.jpg`" alt="Card image cap">
+                    <p class="card-text">Picture 1</p>
                   </card>
                 </div>
               </div>
+              <div class="d-flex justify-content-between align-items-center">
+                  <base-button class="animation-on-hover" type="primary">Reserve</base-button>
+                  <base-button class="animation-on-hover" type="danger">Cancel</base-button>
+                  <base-button class="animation-on-hover" type="success">Comment</base-button>
+              </div>
+              <div class="dropdown-divider"></div>
+              <div slot="footer" class="text-muted">
+                SUSTech
+              </div>
+            </div>
+            <card>
+              <base-input class="has-success" value="Comment here..."></base-input>
             </card>
-          </div>
+            <div>
+              <div v-for="(comment, cIndex) in event.comments" :key="cIndex">
+                <card class="mb-3">
+                  <h4 class="card-title">{{ comment.username }}</h4>
+                  <p class="card-text">{{ comment.comment }}</p>
+                  <p class="card-text"><small class="text-muted">{{ comment.time }}</small></p>
+                </card>
+              </div>
+            </div>
+          </card>
+        </div>
       </fade-transition>
     </div>
   </div>
