51443f542a305bf9fd4747287be6737e3911110b
Yujing27 <12111944@mail.sustech.edu.cn>
2024-04-29 16:16:50 +0800

    add white-background version

--- FrontEnd/src/pages/Dashboard.vue
+++ FrontEnd/src/pages/Dashboard.vue
@@ -19,56 +19,6 @@
       </side-bar>
     </div>
     <div class="row">
-<!--      <div class="col-12">-->
-<!--        <card type="chart">-->
-<!--          <template slot="header">-->
-<!--            <div class="row">-->
-<!--              <div class="col-sm-6" :class="isRTL ? 'text-right' : 'text-left'">-->
-<!--                <h5 class="card-category">-->
-<!--                  {{ $t("dashboard.totalShipments") }}-->
-<!--                </h5>-->
-<!--                <h2 class="card-title">{{ $t("dashboard.performance") }}</h2>-->
-<!--              </div>-->
-<!--              <div class="col-sm-6">-->
-<!--                <div-->
-<!--                  class="btn-group btn-group-toggle"-->
-<!--                  :class="isRTL ? 'float-left' : 'float-right'"-->
-<!--                  data-toggle="buttons"-->
-<!--                >-->
-<!--                  <label-->
-<!--                    v-for="(option, index) in bigLineChartCategories"-->
-<!--                    :key="option"-->
-<!--                    class="btn btn-sm btn-primary btn-simple"-->
-<!--                    :class="{ active: bigLineChart.activeIndex === index }"-->
-<!--                    :id="index"-->
-<!--                  >-->
-<!--                    <input-->
-<!--                      type="radio"-->
-<!--                      @click="initBigChart(index)"-->
-<!--                      name="options"-->
-<!--                      autocomplete="off"-->
-<!--                      :checked="bigLineChart.activeIndex === index"-->
-<!--                    />-->
-<!--                    {{ option }}-->
-<!--                  </label>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </template>-->
-<!--          <div class="chart-area">-->
-<!--            <line-chart-->
-<!--              style="height: 100%"-->
-<!--              ref="bigChart"-->
-<!--              chart-id="big-line-chart"-->
-<!--              :chart-data="bigLineChart.chartData"-->
-<!--              :gradient-colors="bigLineChart.gradientColors"-->
-<!--              :gradient-stops="bigLineChart.gradientStops"-->
-<!--              :extra-options="bigLineChart.extraOptions"-->
-<!--            >-->
-<!--            </line-chart>-->
-<!--          </div>-->
-<!--        </card>-->
-<!--      </div>-->
     </div>
 
     <div class="row">
@@ -78,7 +28,7 @@
             <img slot="image" class="card-img-top" :src="getEventImagePath(index)" :alt="card.title" style="width: 60rem; height: 16rem;" />
             <h4 class="card-title">{{ card.title }}</h4>
             <p class="card-text">{{ card.description }}</p>
-            <base-button tag="a" type="primary" :href="getEventUrlPath(index)" role="button" aria-pressed="true"
+            <base-button tag="a" simple type="primary" :href="getEventUrlPath(index)" role="button" aria-pressed="true"
               class="animation-on-hover btn-center"> Event Detail
             </base-button>
           </card>
@@ -160,91 +110,6 @@ export default {
           link: '#'
         },
       ],
-      bigLineChart: {
-        allData: [
-          [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
-          [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
-          [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130],
-        ],
-        activeIndex: 0,
-        chartData: {
-          datasets: [{}],
-          labels: [
-            "JAN",
-            "FEB",
-            "MAR",
-            "APR",
-            "MAY",
-            "JUN",
-            "JUL",
-            "AUG",
-            "SEP",
-            "OCT",
-            "NOV",
-            "DEC",
-          ],
-        },
-        extraOptions: chartConfigs.purpleChartOptions,
-        gradientColors: config.colors.primaryGradient,
-        gradientStops: [1, 0.4, 0],
-        categories: [],
-      },
-      purpleLineChart: {
-        extraOptions: chartConfigs.purpleChartOptions,
-        chartData: {
-          labels: ["JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
-          datasets: [
-            {
-              label: "Data",
-              fill: true,
-              borderColor: config.colors.primary,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.primary,
-              pointBorderColor: "rgba(255,255,255,0)",
-              pointHoverBackgroundColor: config.colors.primary,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [80, 100, 70, 80, 120, 80],
-            },
-          ],
-        },
-        gradientColors: config.colors.primaryGradient,
-        gradientStops: [1, 0.2, 0],
-      },
-      greenLineChart: {
-        extraOptions: chartConfigs.greenChartOptions,
-        chartData: {
-          labels: ["JUL", "AUG", "SEP", "OCT", "NOV"],
-          datasets: [
-            {
-              label: "My First dataset",
-              fill: true,
-              borderColor: config.colors.danger,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.danger,
-              pointBorderColor: "rgba(255,255,255,0)",
-              pointHoverBackgroundColor: config.colors.danger,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [90, 27, 60, 12, 80],
-            },
-          ],
-        },
-        gradientColors: [
-          "rgba(66,134,121,0.15)",
-          "rgba(66,134,121,0.0)",
-          "rgba(66,134,121,0)",
-        ],
-        gradientStops: [1, 0.4, 0],
-      },
       blueBarChart: {
         extraOptions: chartConfigs.barChartOptions,
         chartData: {
