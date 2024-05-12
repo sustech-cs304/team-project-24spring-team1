e1954b2399f2973c4ed84fe5e1c6d565d231be87
Mandyyymeng <2952455710@qq.com>
2024-05-12 21:22:59 +0800

    a

---
+++ FrontEnd/src/layout/admin/AdminLayout.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+      <dashboard-content @click.native="toggleSidebar"> </dashboard-content>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import DashboardContent from "./Content.vue";
+import {BaseTable} from "@/components";
+import 'element-ui/lib/theme-chalk/index.css';
+import dialogInd from "@/adminform/components/dialogInd.vue";
+
+export default {
+  name: 'AdminLayout',
+  components: {
+    BaseTable,
+    TopNavbar,
+    DashboardContent,
+  },
+  data() {
+    return {
+      imgUrl:require("@/adminform/assets/th.jpg")
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+.background{
+  width:110%;
+  height:120%;
+  z-index:-1;
+  position: absolute;
+  opacity: 0.8;
+}
+
+.wrapper {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
