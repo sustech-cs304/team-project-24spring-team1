6d73c8a4423cec9dc76e7912fb9d678f23382313
Mandyyymeng <2952455710@qq.com>
2024-05-24 13:24:56 +0800

    topnavbar

--- FrontEnd/testBackend.py
+++ FrontEnd/testBackend.py
@@ -1,16 +1,33 @@
 import requests
 
-url = "https://backend.sustech.me/api/auth/register"
+# url = "https://backend.sustech.me/api/auth/register"
+url_places = "https://backend.sustech.me/api/metadata/places"
+url_login = "https://sso.cra.ac.cn/realms/cra-service-realm/protocol/cas/login?service=https://backend.sustech.me/api/auth/callback?identifier=${identifier}"
+url_create = "https://backend.sustech.me/api/event"
+
 payload = {
-    "sustech_id": 12111611,
-    "name": "RuixiangJiang",
+    "sustech_id": 12110001,
+    "name": "team1",
     "password": "jrxjrxjrx"
 }
 headers = {
     "Content-Type": "application/json"
 }
 
-response = requests.post(url, json=payload)
+activity = {
+    "name": "Event Name",
+    "kind": "show",
+    "description": "Event Description",
+    "venue_id": 1,
+    "start_at": "2021-05-21T12:00:00",
+    "end_at": "2021-05-21T14:00:00",
+    "tickets": 100 ,
+    "registration_deadline": "2021-05-21T11:00:00"
+}
+
+# response = requests.post(url_create, json= activity)
+# response = requests.get(url_login)
+response = requests.get(url_create)
 
 if response.status_code == 200:
     print("Success Message: ", response.text)
