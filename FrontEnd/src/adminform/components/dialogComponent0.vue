28f9ff70b4e370a06f429c45d034351a66553c78
Mandyyymeng <2952455710@qq.com>
2024-05-24 21:21:23 +0800

    aborted change

---
+++ FrontEnd/src/adminform/components/dialogComponent0.vue
@@ -0,0 +1,423 @@
+<template>
+  <!-- 编辑活动的窗口 -->
+  <transition name="dialog-fade">
+    <el-dialog
+        v-if="showDialog"
+        :title="dialogTitle"
+        class="dialog-component"
+        :visible.sync="showDialog"
+        width="600px"
+        @close="closeDialog(0)"
+    >
+
+      <el-form
+          ref="formInfo"
+          :model="formInfo"
+          :rules="formInfoRules"
+          class="demo-form-inline"
+          label-width="100px"
+          :inline="true" >
+
+        <el-form-item label="activityName" prop="activityName">
+          <el-input v-model="formInfo.activityName" placeholder="activity name" clearable/>
+        </el-form-item>
+
+        <div style="display:flex;flex-wrap:wrap;">
+        <el-form-item label="kind" prop="kind">
+          <el-radio v-model="formInfo.kind" label="show">show</el-radio>
+          <el-radio v-model="formInfo.kind" label="lecture">lecture</el-radio>
+          <el-radio v-model="formInfo.kind" label="competition">competition</el-radio>
+          <el-radio v-model="formInfo.kind" label="other">other</el-radio>
+        </el-form-item>
+        </div>
+
+<!--        <el-form-item label="date" prop="date" label-width="150px">-->
+<!--          <el-date-picker-->
+<!--              v-model="formInfo.date"-->
+<!--              type="date"-->
+<!--              placeholder="Pick a date"-->
+<!--              value-format="yyyy-MM-dd"-->
+<!--              style="width: 250px"-->
+<!--              :picker-options="dateOptions"-->
+<!--              clearable-->
+<!--          />-->
+<!--        </el-form-item>-->
+
+        <el-form-item prop="startTime">
+          <el-date-picker
+              v-model="formInfo.startTime"
+              :clearable="true"
+              type="datetime"
+              placeholder="startTime">
+          </el-date-picker>
+<!--          picker-options="{&ndash;&gt;-->
+          <!--              start: '00:00',-->
+          <!--              step: '00:01',-->
+          <!--              end:formInfo.startTime?formInfo.startTime:'23:59',-->
+          <!--              maxTime: formInfo.endTime-->
+          <!--          }">-->
+        </el-form-item>
+        <span class="">-</span>
+
+        <el-form-item prop="endTime">
+        <el-date-picker
+            style="width:200px;"
+            placeholder="endTime"
+            type="datetime"
+            :clearable="false"
+            v-model="formInfo.endTime"
+            :picker-options="{
+              // start: formInfo.endTime?param.endTime:'00:00',
+              // step: '00:01',
+              // end:'24:00',
+              // minTime: formInfo.startTime
+            }">
+        </el-date-picker>
+        </el-form-item>
+
+<!--        <el-form-item label="venue_id" prop="venue_id">-->
+<!--          <el-select-->
+<!--              v-model="formInfo.venue_id"-->
+<!--              placeholder="venue_id"-->
+<!--              clearable-->
+<!--          >-->
+<!--            <el-option label="&#45;&#45;" value="none" />-->
+<!--            <el-option label="Teaching Building No.1" value=1 />-->
+<!--            <el-option label="Lecture Hall" value=2 />-->
+<!--            <el-option label="Research Building Lecture Hall" value=3 />-->
+<!--            <el-option label="Library Conference Hall" value=4 />-->
+<!--            <el-option label="south Building" value=5 />-->
+<!--          </el-select>-->
+<!--        </el-form-item>-->
+
+        <el-form-item label="venue_id" prop="venue_id">
+          <el-input v-model="formInfo.venue_id" placeholder="venue_id" clearable :pattern="'^[0-9]+$'" />
+        </el-form-item>
+
+        <el-form-item label="tickets" prop="tickets">
+          <el-input v-model="formInfo.tickets" placeholder="tickets available" clearable :pattern="'^[0-9]+$'" />
+        </el-form-item>
+
+        <el-form-item label="UPLOAD IMAGE" prop="images">
+          <el-upload
+              action=""
+              :limit="3"
+              list-type="picture-card"
+              :on-exceed="handleExceed"
+              :before-upload="beforeUpload"
+              :on-remove="handleRemove"
+              :file-list="fileList"
+          >
+            <i class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+
+        <el-form-item prop="deadline">
+          <el-date-picker
+              v-model="formInfo.deadline"
+              :clearable="true"
+              type="datetime"
+              placeholder="deadline">
+          </el-date-picker>
+        </el-form-item>
+
+        <el-form-item label="description" prop="description">
+          <el-input v-model="formInfo.description" placeholder="description of your activity"
+                    clearable :pattern="'^[0-9]+$'" />
+        </el-form-item>
+
+<!--        <el-form-item>-->
+<!--          <el-upload-->
+<!--              action="https://jsonplaceholder.typicode.com/posts/"-->
+<!--              :http-request="onUpload"-->
+<!--          >-->
+<!--            <el-button size="small" type="primary">CLICK TO UPLOAD</el-button>-->
+<!--          </el-upload>-->
+<!--        </el-form-item>-->
+
+        <el-form-item style="text-align: right;">
+          <el-button type="primary" @click="submitForm('formInfo')" size="medium">confirm publish</el-button>
+          <el-button type="primary" @click="submitFormEdit('formInfo')" size="medium">submit change</el-button>
+          <el-button type="primary" @click="closeDialog(0)">cancel</el-button>
+          <el-button type="primary" @click="clearEntry">clear</el-button>
+        </el-form-item>
+
+      </el-form>
+    </el-dialog>
+  </transition>
+</template>
+
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "DialogComponent",
+  //components:{dateP},
+  props: {
+    dialogTitle: {
+      type: String,
+      default: "publish event",
+    },
+    itemInfo: {
+      type: Object,
+      default: function () {
+        return {};
+      },
+    },
+  },
+
+  data() {
+    return {
+      showDialog: false,
+      formInfo: JSON.parse(JSON.stringify(this.itemInfo)),
+      formInfoRules:{
+        acitivityName: [
+          { required:true,message: "please enter activity name", trigger: "blur" },
+        ],
+        kind: [{required:true}],
+        venue_id:[{ required:true ,message:"venue",trigger:blur}],
+        startTime: [{required:true,message:"choose start time",trigger:blur}],
+        endTime: [{ required:true,message:"choose end time",trigger:blur},],
+        description: [{required: false, message: "enter description", trigger: "blur" },],
+        tickets:[{required:true,message:"tickets available",trigger:"blur"},
+          {
+            pattern: /^\d+$/,
+            message: "number of tickets, no less than 0",
+            trigger: "blur"
+          }],
+        deadline: [{required:true, trigger:"blur"}],
+      },
+      dateOptions: {
+        disabledDate(time) {
+          return time.getTime() < Date.now() - 8.64e7;
+        }
+      },
+    };
+  },
+
+  methods: {
+    submitForm(formName) {
+      const that = this;
+      //const params = Object.assign(this.formInfo, {});
+      that.$refs[formName].validate((valid) => {
+        if (valid) {
+          //console.log(this.dialogTitle) console.log(that.formInfo)
+          const start_iso = new Date(this.formInfo.startTime).toISOString();
+          const end_iso = new Date(this.formInfo.endTime).toISOString();
+          const deadline_iso = new Date(this.formInfo.deadline).toISOString();
+          const eventData={
+            name:this.formInfo.activityName,
+            kind:this.formInfo.kind,
+            description: this.formInfo.description,
+            venue_id:parseInt(this.formInfo.venue_id, 10), // 将字符串转换为i32类型 10进制
+            start_at:start_iso.substring(0, start_iso.length - 5),
+            end_at:end_iso.substring(0, end_iso.length - 5), //"2021-05-21T14:00:00",
+            tickets: parseInt(this.formInfo.tickets), //100 | null,
+            registration_deadline: deadline_iso.substring(0, start_iso.length - 5), //"2021-05-21T11:00:00" | null
+          }
+          // this.imageUrl = `https://backend.sustech.me/uploads/${userData.avatar}.webp`;
+          this.token = localStorage.getItem('token');
+          if (!this.token) {
+            console.log("Token not found.");
+            return;
+          }
+
+          const apiUrl = `https://backend.sustech.me/api/event`;
+          axios.post(apiUrl, eventData,
+              {timeout: 3000,headers: {
+                  Authorization: `Bearer ${this.token}`
+                }
+              })
+              .then(response => {
+                that.$message({
+                  message: "发布成功",
+                  type: "success",
+                });
+                this.$emit("pushDialogData",eventData);
+                eventData.id = response.data.id;
+              })
+              .catch(error => {
+                console.error('failed to publish event：', error);
+                this.showFailMessage(`发布活动失败`);
+              })
+          that.closeDialog(1);
+        } else {
+          return false;
+        }
+      });
+    },
+
+    showSuccessMessage(message) {
+      this.successMessage = message;
+      this.$message({
+        message: this.successMessage,
+        type: 'success'
+      });
+    },
+    showFailMessage(message) {
+      this.Message = message;
+      this.$message({
+        message: this.Message,
+        type: 'error'
+      });
+    },
+    handleRemove(file) {
+      this.fileList = this.fileList.filter(item => item.uid !== file.uid);
+    },
+    handleExceed() {
+      this.showFailMessage("最多只能传3张照片");
+    },
+    beforeUpload(file) {
+      const isJPG = file.type === "image/jpeg" || file.type == "image/png";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 或 PNG 格式!");
+        return;
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+        return;
+      }
+      const fileData = new FormData();
+      fileData.append("avatar", file);
+      //upload为上传的接口
+      upload(fileData).then(res => {
+        this.imgUrl = res.imgUrl;
+        //对返回的图片地址进行回显
+        this.$set(this.form, "avatar", this.imgUrl);
+      });
+      //阻止传到本地浏览器
+      return false;
+    },
+
+    submitFormEdit(formName) {
+      const that = this;
+      //const params = Object.assign(this.formInfo, {});
+      that.$refs[formName].validate((valid) => {
+        if (valid) {
+          //console.log(this.dialogTitle) console.log(that.formInfo)
+          const start_iso = new Date(this.formInfo.startTime).toISOString();
+          const end_iso = new Date(this.formInfo.endTime).toISOString();
+          const deadline_iso = new Date(this.formInfo.deadline).toISOString();
+          const eventData={
+            name:this.formInfo.activityName,
+            kind:this.formInfo.kind,
+            description: this.formInfo.description,
+            venue_id:parseInt(this.formInfo.venue_id, 10), // 将字符串转换为i32类型 10进制
+            start_at:start_iso.substring(0, start_iso.length - 5),
+            end_at:end_iso.substring(0, end_iso.length - 5), //"2021-05-21T14:00:00",
+            tickets: parseInt(this.formInfo.tickets), //100 | null,
+            registration_deadline: deadline_iso.substring(0, start_iso.length - 5), //"2021-05-21T11:00:00" | null
+          }
+          // this.imageUrl = `https://backend.sustech.me/uploads/${userData.avatar}.webp`;
+          this.token = localStorage.getItem('token');
+          if (!this.token) {
+            console.log("Token not found.");
+            return;
+          }
+
+          const apiUrl = `https://backend.sustech.me/api/event`;
+          axios.post(apiUrl, eventData, {timeout: 3000,headers: {
+                  Authorization: `Bearer ${this.token}`
+                }
+              })
+              .then(response => {
+                that.$message({
+                  message: "发布成功",
+                  type: "success",
+                });
+                this.$emit('editDialog',eventData)
+                eventData.id = response.data.id;
+              })
+              .catch(error => {
+                console.error('failed to publish event：', error);
+                this.showFailMessage(`发布活动失败`);
+              })
+          this.$parent.tableData[this.$parent.editrowNum]=eventData;
+          that.closeDialog(1);
+
+        } else {
+          return false;
+        }
+      });
+
+    },
+
+    closeDialog(flag) {
+      this.showDialog = false;
+      this.$emit("closeDialog", flag);
+      this.$refs["formInfo"].resetFields();
+    },
+    clearEntry() {
+      //this.formInfo="";
+      this.$refs["formInfo"].resetFields();
+    },
+
+  },
+};
+
+</script>
+
+
+
+<style scoped lang="scss">
+
+.dialog-fade-enter-active {
+  -webkit-animation: dialog-fade-in 0.3s;
+  animation: dialog-fade-in 0.3s;
+}
+.dialog-fade-leave-active {
+  -webkit-animation: dialog-fade-out 0.3s;
+  animation: dialog-fade-out 0.3s;
+}
+@-ms-keyframes dialog-fade-in {
+  0% {
+    -ms-transform: translate3d(0, -20px, 0);
+    transform: translate3d(0, -20px, 0);
+    opacity: 0;
+  }
+  100% {
+    -ms-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+}
+@keyframes dialog-fade-in {
+  0% {
+    -ms-transform: translate3d(0, -20px, 0);
+    transform: translate3d(0, -20px, 0);
+    opacity: 0;
+  }
+  100% {
+    -ms-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+}
+@-ms-keyframes dialog-fade-out {
+  0% {
+    -ms-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+    /*@-webkit-*/
+  }
+  100% {
+    -ms-transform: translate3d(0, -20px, 0);
+    transform: translate3d(0, -20px, 0);
+    opacity: 0;
+  }
+}
+@keyframes dialog-fade-out {
+  0% {
+    -ms-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+  100% {
+    -ms-transform: translate3d(0, -20px, 0);
+    transform: translate3d(0, -20px, 0);
+    opacity: 0;
+  }
+}
+</style>
---
+++ FrontEnd/src/adminform/components/dialogInd0.vue
@@ -0,0 +1,322 @@
+<template>
+  <!-- 显示的大表格 -->
+  <div class="dialog-demo">
+<!--       :style="{ backgroundImage:  'url(' + imgUrl + ')' }">-->
+    <p class="title"> Activity Management </p>
+    <el-row style="margin-top: 25px;margin-bottom: 10px">
+      <base-button type="primary" @click="addItem" size="medium">PUBLISH</base-button>
+      <base-button type="primary" @click="deleteSelectedRows" size="medium" style="margin-left:40px;">
+        delete selection</base-button>
+      <base-button type="primary" size="medium" @click="deleteAllRows" style="margin-left:40px;">
+        delete all </base-button>
+    </el-row>
+
+    <el-table
+        v-loading="tableLoading"
+        :data="tableData.slice((currentPage-1)*pageSize,currentPage*pageSize)"
+        :page-size="5" stripe pagination
+        class="storeTable"
+        style="width: 100%; margin-top: 30px;"
+        @selection-change="handleSelectionChange"
+    >
+      <!--"v-for="item in tableItem" :key="checked"-->
+
+      <el-table-column label="select" width="100" type="selection"></el-table-column>
+      <el-table-column label="id" prop="id" width="80"></el-table-column>
+      <el-table-column label="activityName" prop="activityName" width="110"></el-table-column>
+      <el-table-column label="kind" prop="kind" width="105"></el-table-column>
+      <el-table-column label="venue_id" prop="venue_id" width="110"></el-table-column>
+      <el-table-column label="startTime" prop="startTime" width="110"></el-table-column>
+      <el-table-column label="endTime" prop="endTime" width="110"></el-table-column>
+<!--      <el-table-column label="deadline" prop="duration" width="100"></el-table-column>-->
+      <el-table-column label="description" prop="description" width="150"></el-table-column>
+
+      <el-table-column label="operation" width="100">
+        <template slot-scope="scope">
+          <base-button type="primary" size=sm id="editBtn"
+                     @click.native.prevent="editRow(scope.$index,scope.row)" round>EDIT</base-button>
+          <base-button type="primary" size=sm id="deleteBtn" style="background-color: red"
+                     @click.native.prevent="deleteRow(scope.$index,true)" round>DEL</base-button>
+        </template>
+      </el-table-column>
+
+    </el-table>
+
+      <div class="block" style="margin-top:15px;">
+        <el-pagination align='center'
+                       @size-change="handleSizeChange"
+                       @current-change="handleCurrentChange"
+                       :current-page="currentPage"
+                       :page-sizes="[3,5,10]"
+                       :page-size="pageSize"
+                       layout="total, sizes, prev, pager, next, jumper"
+                       :total="tableData.length">
+        </el-pagination>
+      </div>
+
+    <dialog-component
+        v-if="showDialog"
+        ref="dialogComponent"
+        :dialog-title="dialogTitle"
+        :item-info="tableItem"
+        :sendData="ifE"
+        @closeDialog ="closeDialog"
+        @pushDialogData="updateTable"
+        @editDialog="updateEdit"
+    ></dialog-component>
+
+  </div>
+</template>
+
+
+<script>
+import DialogComponent from "@/adminform/components/dialogComponent.vue";
+import axios from "axios";
+import event from "@/pages/Event/Event.vue";
+
+export default {
+  name: "DialogDemo",
+  components: { DialogComponent},
+  data() {
+    return {
+      tableLoading: false,
+      showDialog: false,
+      dialogTitle: "PUBLISH EVENT",
+      pageSize:5,
+      currentPage: 1,
+      ifE: false,
+      editrowNum:'0',
+      tableData: [
+        // {activityName:'ddd', kind:'Free', venue_id:'1', description:'4'},
+        // {activityName:'ddd', kind:'Free', venue_id:'3', description:'4'},
+      ],
+      tableItem: {
+        activityName:'',
+        description:'',
+        kind:'',
+        venue_id:0,
+        startTime:'2023-03-02 08:08',
+        endTime:'',
+        deadline: '',
+        id: 0,
+      },
+    };
+  },
+
+  mounted() {
+    this.fetchData();
+  },
+  computed: {
+    filteredTableData() {
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+      return this.tableData.slice(startIndex, endIndex);
+    }
+  },
+
+  methods: {
+    fetchData() {
+      const that = this;
+      that.tableLoading = true;
+      setTimeout(() => {
+        that.tableLoading = false;
+      }, 1000);
+      const apiUrl = "https://backend.sustech.me/api/event"
+      axios.get(apiUrl, {timeout: 3000})
+          .then(response => {
+            const events = response.data.events;
+            this.tableData = [];
+            for (const event of events) {
+              let get_event = {};
+              get_event.id = event.id;
+              get_event.activityName = event.name;
+              get_event.kind = event.kind;
+              get_event.description = event.description;
+              get_event.startTime = event.start_at;
+              get_event.endTime = event.end_at; //string
+              get_event.venue_id = event.venue.id; //string
+              get_event.tickets = event.tickets;
+              get_event.deadline = event.registeration_deadline;
+              // console.log("event: ",event);
+              this.tableData.push(get_event);
+            }
+          })
+          .catch(error => {
+            console.error('failed to init activities：', error);
+            this.showFailMessage(`活动初始化失败`);
+          })
+    },
+    handleSizeChange(val) {
+      console.log(`${val} records per page`);
+      this.currentPage = 1;
+      this.pageSize = val;
+    },
+    //当前页改变时触发 跳转其他页
+    handleCurrentChange(val) {
+      console.log(`current page: ${val}`);
+      this.currentPage = val;
+    },
+
+    addItem() {
+      this.tableItem={
+        activityName:'',
+        description:'',
+        kind:'',
+        venue_id:'',
+        tickets:'',
+        startTime:'',
+        endTime:'',
+        deadline:'',
+        id:'',
+      };
+      this.dialogTitle = "publish";
+      this.showDialog = true;
+      this.$nextTick(() => {
+        this.$refs["dialogComponent"].showDialog = true;
+      });
+    },
+
+    editRow(ind,row) {
+      this.tableItem = row;
+      this.dialogTitle = "edit";
+      this.ifE=true;
+      this.editrowNum=ind;
+      this.showDialog = true;
+      this.$nextTick(() => {
+        this.$refs["dialogComponent"].showDialog = true;
+        //this.$refs["dialogComponent"].father_ifE=true;
+      });
+    },
+
+    updateTable(data){
+      this.tableData.push(data);
+    },
+    updateEdit(data){
+      //this.tableData[this.editrowNum]=data;
+      console.log("edit")
+      console.log(this.editrowNum)
+      this.tableData.splice(this.editrowNum,1,data)
+      this.$nextTick(() => {
+      });
+      console.log(this.tableData)
+    },
+    closeDialog(flag) {
+      if (flag) {
+        this.fetchData();
+      }
+      this.showDialog = false;
+    },
+
+    deleteRow(index,ifMsg){
+      // this.tableData.splice(index, 1);
+      const event_id = this.tableData.at(index).id;
+      const deleteUrl = `https://backend.sustech.me/api/event/${event_id}`
+      axios.delete(deleteUrl, {timeout: 3000,headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+          })
+          .then(response => {
+            if (ifMsg) {
+              this.$message({
+                message: `删除成功`,
+                type: "success",
+              });
+            }
+          })
+          .catch(error => {
+            console.error('failed to delete event：', error);
+            this.showFailMessage(`删除活动失败`);
+            return false;
+          })
+      return true;
+    },
+    showFailMessage(message) {
+      this.Message = message;
+      this.$message({
+        message: this.Message,
+        type: 'error'
+      });
+    },
+
+    handleSelectionChange(selection) {
+      this.selectedRows = selection;
+    },
+    deleteSelectedRows() {
+      const indices = this.selectedRows.map(row => this.tableData.indexOf(row));
+      // if (indices.length > 0) {
+      //   this.tableData = this.tableData.filter((_, index) => !indices.includes(index));
+      // }
+      let check = true;
+      for (const idx of indices){
+        check = check && this.deleteRow(idx,false);
+      }
+      if (check) {
+        this.showSuccessMessage(`删除选择部分活动成功`);
+      }else{
+        this.showFailMessage(`不能删除非您发布的活动`);
+      }
+    },
+    showSuccessMessage(message) {
+      this.successMessage = message;
+      this.$message({
+        message: this.successMessage,
+        type: 'success'
+      });
+    },
+    deleteAllRows() {
+      // this.tableData = [];
+      let check = true;
+      for (let i = 0; i < this.tableData.length; i++) {
+        check= check && this.deleteRow(i, false);
+      }
+      if (!check) {
+        this.showFailMessage(`不能删除非您发布的活动`);
+      }
+    },
+
+  },
+};
+</script>
+
+
+<style scoped lang="scss">
+.dialog-demo{
+  position: absolute;
+  padding: 50px 115px;
+  justify-content: left; // center
+  .instructions {
+    font-size: 20px;
+    padding: 10px 0;
+    color: #304455;
+  }
+  .desc-list {
+    padding: 10px 0 30px 40px;
+    line-height: 30px;
+    font-size: 15px;
+    color: #606266;
+    list-style-type: disc;
+  }
+}
+
+.storeTable{
+  width: 210px;
+  background-color: rgba(255, 255, 255,0.6);
+  opacity: 0.95;
+}
+
+  .title{
+    animation-name: colorChange;
+    animation-duration: 6s;
+    animation-iteration-count: infinite;
+    font-size: 35px;
+    font-weight: bolder;
+    //text-shadow: -3px -3px 0px rgba(0, 0, 255, 0.2),
+  }
+
+  @keyframes colorChange {
+    0% { color: #b895d5; }
+    50% { color: #a16cc5; }
+    100% { color: #805bb6; }
+  }
+
+</style>
