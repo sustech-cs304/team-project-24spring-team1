c025801dbc5c592eea551f6db3c4503d4247d6bf
Hanziyu12 <12112841@mail.sustech.edu.cn>
2024-05-24 22:45:17 +0800

    update myevent and mymoment, connect to backend

--- FrontEnd/src/pages/MyEvent.vue
+++ FrontEnd/src/pages/MyEvent.vue
@@ -11,16 +11,17 @@
 <template>
     <div>
         <div class="row">
+            <!-- <card v-for="event in eventData" :key="event.id" type="nav-tabs" class="text-center"></card> -->
             <card v-for="event in eventData" :key="event.id" type="nav-tabs" class="text-center">
                 <div slot="header" class="card-header-primary">
-                    Hold by {{ event.holder }}
+                    Hold by {{ event.organizer.name }}
                 </div>
-                <h4 class="card-title">{{ event.title }}</h4>
-                <p class="card-text">{{ event.content }}</p>
+                <h4 class="card-title">{{ event.name }}</h4>
+                <p class="card-text">{{ event.description }}</p>
                 <!-- <a href="#" class="btn btn-primary">Check</a> -->
                 <a :href="'#/event/' + event.id" class="btn btn-primary">Check</a>
                 <div slot="footer" class="text-muted">
-                    {{ calculateTime(event.time) }}
+                    {{ calculateTime(event.start_at) }}
                 </div>
             </card>
         </div>
@@ -34,27 +35,82 @@
 </template>
     
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
-            eventData: [
-                {
-                    id: 1,
-                    holder: "holder 1",
-                    title: "title 1",
-                    content: "content 1",
-                    time: "2024-05-31",
-                },
-                {
-                    id: 2,
-                    holder: "holder 2",
-                    title: "title 2",
-                    content: "content 2",
-                    time: "2024-05-20",
-                },
-            ]
-        }
+            eventData: [], // 将 eventData 定义为数组
+            }
+
+        // return {
+        //     eventData: [
+        //         {
+        //             id: 1,
+        //             holder: "holder 1",
+        //             title: "title 1",
+        //             content: "content 1",
+        //             time: "2024-05-31",
+        //         },
+        //         {
+        //             id: 2,
+        //             holder: "holder 2",
+        //             title: "title 2",
+        //             content: "content 2",
+        //             time: "2024-05-20",
+        //         },
+        //     ]
+        // }
     },
+    mounted() {
+        this.i18n = this.$i18n;
+        if (this.enableRTL) {
+        this.i18n.locale = "ar";
+        this.$rtl.enableRTL();
+        }
+        const list_event_api = 'https://backend.sustech.me/api/event'
+        axios.get(list_event_api, {
+        headers: {
+        }
+        })
+            .then(response => {
+            const eventData = response.data;
+            this.eventData = eventData.events;
+            // this.eventData.organizer = eventData.events.organizer;
+            })
+            .catch(error => {
+            console.error('Error fetching profile data:', error);
+            });
+    },
+    // mounted() {
+    //     this.token = localStorage.getItem('token');
+    //     if (!this.token) {
+    //         console.log("Token not found.");
+    //         return;
+    //     }
+
+    //     this.eventData.id = 2;
+    //     const apiUrl = `https://backend.sustech.me/api/event/${this.eventData.id}`; // 修正 eventId 
+
+    //     axios.get(apiUrl, {
+    //         headers: {
+    //         }
+    //     })
+    //     .then(response => {
+    //         const eventData = response.data;
+    //         this.eventData.push({
+    //             id: eventData.id,
+    //             name: eventData.name,
+    //             description: eventData.description,
+    //             organizer: eventData.organizer,
+    //             start_at: eventData.start_at,
+    //             end_at: eventData.end_at,
+    //         });
+    //     })
+    //     .catch(error => {
+    //         console.error('Error fetching profile data:', error);
+    //     });
+    // },
     computed: {
         upcomingEventsCount() {
             const currentTime = new Date();
@@ -77,7 +133,21 @@ export default {
             } else {
                 return "in " + daysDiff + " days";
             }
-        }
+        },
+        getEventById(eventId) {
+        axios.get(`https://backend.sustech.me/api/event/${eventId}`) // 发送 GET 请求到指定 eventId 的事件数据接口
+            .then(response => {
+                // 更新特定 eventId 的事件数据
+                const eventData = response.data;
+                const existingEventIndex = this.eventData.findIndex(event => event.id === eventId);
+                if (existingEventIndex !== -1) {
+                    this.$set(this.eventData, existingEventIndex, eventData);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching event data:', error);
+            });
+    }
     },
 }
 </script>
--- FrontEnd/src/pages/MyMoment.vue
+++ FrontEnd/src/pages/MyMoment.vue
@@ -1,26 +1,84 @@
 <style>
-    .notification {
-        position: absolute;
-        top: 17px;
-        right: 120px;
-        width: 6px;
-        height: 6px;
-    }
+.image-container {
+    display: flex;
+    justify-content: center; /* 水平居中 */
+    align-items: center; /* 垂直居中 */
+    width: 100%; 
+    height: 100%; 
+}
+.card-text {
+    margin-bottom: 20px;
+}
+.name {
+    font-size: 20px; 
+}
+.role {
+    margin-left: 5px; 
+}
+.created-time {
+    float: right; 
+}
+.comment-container {
+    text-align: center; 
+}
+
+.comment-input-wrapper {
+    display: flex;
+    align-items: center;
+    width: 100%;
+}
+
+.comment-button {
+    margin-left: 20px; 
+}
+
+
 </style>
 <template>
     <div>
         <div class="row">
             <div class="col-lg-4" :class="{ 'text-right': isRTL }">
-                <card v-for="(card, index) in moment" :key="index" style="width: 50rem;">
+                <card v-for="(card, index) in moments" :key="index" style="width: 50rem;">
+                    <!-- <p :style="{ color: 'black' }">Moment ID: {{ card.id }}</p> -->
+                    <div class="card-text">
+                        <span class="name">{{ card.account.name }}</span> | <span class="role">{{ card.account.role }}</span>
+                    </div>
+                    
+                    <div class="card-text">{{ card.content }}</div>
+
                     <template v-for="(imageNumber, i) in [1]">
-                        <img slot="image" class="card-img-top" :key="i" :src="getMomentImagePath(card.id, imageNumber)" alt="Card image cap"/>
+                        <div class="image-container">
+                            <img slot="image" class="card-img-top" :key="i" :src="getMomentImagePath(card.id, imageNumber)" alt="Card image cap" style="width: 400px; height: auto;"/>
+                        </div>
                     </template>
-                    <p class="card-text">{{ card.content }}</p>
+
+                    <div class="card-text created-time">{{ card.created_at }}</div>
+
+
+                    <div class="comment-container">
+                        <div style="margin-bottom: 40px;"></div> <!-- 空两行 -->
+                        <div class="comment-input-wrapper">
+                            <input type="text" class="form-control" v-model="newComment" placeholder="Comment here..." style="color: black;">
+                            <base-button class="animation-on-hover" style="margin-left: 10px;" @click="postComment(card.id)">Comment</base-button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <base-button class="animation-on-hover" type="success" @click="getComments(card.id)">
+                            <i class="tim-icons icon-chat-33" style="margin-right: 5px;"></i> Show Comments
+                        </base-button>
+                        <div v-for="(comment, cIndex) in comments" :key="cIndex">
+                            <card class="mb-3">
+                                <h4 class="card-title">{{ comment.account.name }}</h4>
+                                <p class="card-text">{{ comment.content }}</p>
+                                <p class="card-text"><small class="text-muted">{{ comment.created_at }}</small></p>
+                            </card>
+                        </div>
+                    </div>
+
                     <div class="d-flex justify-content-around">
-                        <!-- <base-button v-for="(button, bIndex) in card.buttons" :key="bIndex" round icon type="primary" @click="toggleIcon(bIndex)"> -->
                         <base-button v-for="(button, bIndex) in card.buttons" :key="bIndex" round icon type="primary">
-                            <i :class="button.icon"></i>
-                            
+                            <i :class="button.icon"></i>                           
                         </base-button>
                     </div>
                     <div class="dropdown-divider"></div>
@@ -34,65 +92,135 @@
                 </card>
             </div>
         </div>
+        
+
+        <div class="comment-container">
+            <div style="margin-bottom: 40px;"></div>
+            <div class="comment-input-wrapper">
+                <input type="text" class="form-control" v-model="newMoment" placeholder="New moment content here..." style="color: black;">
+            </div>
+        </div>
+        <!-- Post Moment Button -->
+        <div class="row mt-3">
+            <div class="col-lg-4">
+                <button @click="postNewMoment" class="btn btn-primary">Post Moment</button>
+            </div>
+        </div>
     </div>
 </template>
 
 
 <script>
+import axios from 'axios';
 export default {
     props: ['username'],
     data() {
         return {
-            moment: [
-                {
-                    id: 1,
-
-                    content: "Content 1",
-                    buttons: [
-                        { icon: "tim-icons icon-heart-2" },
-                        { icon: "tim-icons icon-chat-33" },                        
-                        //{ icon: "tim-icons icon-simple-delete" }
-                    ],
-                    comments: [
-                        { username: "User 1", comment: "This is a wider card...", time: "Last updated 1 mins ago" },
-                        { username: "User 2", comment: "This is a wider card...", time: "Last updated 10 mins ago" }
-                    ]
-                },
-                {
-                    id: 2,
-                    content: "Content 2",
-                    buttons: [
-                        { icon: "tim-icons icon-heart-2" },
-                        { icon: "tim-icons icon-chat-33" },
-                        //{ icon: "tim-icons icon-simple-delete" }
-                    ],
-                    comments: [
-                        { username: "User 3", comment: "This is a wider card...", time: "Last updated 2 mins ago" },
-                        { username: "User 4", comment: "This is a wider card...", time: "Last updated 5 mins ago" }
-                    ]
-                }
-            ]           
+            moments: [],
+            comments: [],
+            idx:0,
+            momentID: null,
+            userName:null,
+            content: null,
+            time: null,
+            newComment: '',
+            newMoment: '',
+            buttons:[
+                { icon: "tim-icons icon-heart-2" },
+                { icon: "tim-icons icon-chat-33" }, 
+            ]
         }
     },
+    mounted() {       
+        this.fetchMoments(); // 在组件挂载时,调用fetchMoments方法获取数据
+    },
     methods: {
+        postNewMoment() {
+            const commentUrl = `https://backend.sustech.me/api/moment`;
+            const commentData = {
+                content: this.newMoment
+            };
+
+            this.token = localStorage.getItem('token');
+            if (!this.token) {
+                console.log("Token not found.");
+                return;
+            }
+
+            axios.post(commentUrl, commentData, {
+                headers: {
+                Authorization: `Bearer ${this.token}`
+                }
+            })
+                .then(response => {
+                    // Handle successful comment submission
+                    alert('Moment Post successfully.');
+                    
+                    this.newMoment = ''; 
+                    const newMomentId = response.data.id;
+                    
+                    console.log('New Moment ID:', newMomentId);
+                })
+                .catch(error => {
+                    console.error('Error submitting comment:', error);
+                });
+        },
+        fetchMoments() {
+            const commentUrl = `https://backend.sustech.me/api/moment`;
+            axios.get(commentUrl)
+                .then(response => {
+                    // this.momentID = response.data.moments.id;
+                    this.moments = response.data.moments;
+                    // this.momentID = this.moments[this.idx].id;
+                    // this.idx++;                   
+                })
+                .catch(error => {
+                    console.error('Error fetching comments:', error);
+                });
+        },
+
+        postComment(id) {
+            const commentUrl = `https://backend.sustech.me/api/moment/${id}/comment`;
+            const commentData = {
+                content: this.newComment
+            };
+
+            this.token = localStorage.getItem('token');
+            if (!this.token) {
+                console.log("Token not found.");
+                return;
+            }
+
+            axios.post(commentUrl, commentData, {
+                headers: {
+                Authorization: `Bearer ${this.token}`
+                }
+            })
+                .then(response => {
+                    // Handle successful comment submission
+                    alert('Comment submitted successfully.');
+                    this.newComment = ''; // Clear the comment input
+                    // this.getComments(id); // Refresh the comments list
+                })
+                .catch(error => {
+                    console.error('Error submitting comment:', error);
+            });
+        },
+
+        getComments(id) {
+            const commentUrl = `https://backend.sustech.me/api/moment/${id}/comment`;
+            axios.get(commentUrl)
+                .then(response => {
+                    this.comments = response.data.comments;
+                })
+                .catch(error => {
+                    console.error('Error fetching comments:', error);
+            });
+        },
+
         getMomentImagePath(momentId, imageNumber) {
             return `users/testuser/moment/${momentId}-${imageNumber}.jpg`;
         },
-        toggleIcon(bIndex) {
-            if (bIndex === 0) {
-                // 点击第一个按钮，弹出提示
-                alert('You clicked the first button!'); 
-            } else if (bIndex === 1) {
-                // 点击第二个按钮，切换成另一个图标
-                //alert('You clicked the second button!');
-                this.card.buttons[bIndex].icon = "tim-icons icon-satisfied";
-                const userInput = prompt('请输入内容：');
-                if (userInput !== null) {
-                    console.log('用户输入的内容是：', input);
-                }
-            }
-        }
-
     }
 }
 </script>
