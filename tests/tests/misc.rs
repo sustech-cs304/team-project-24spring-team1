d44d87980b9b38b3b410882ecea7c124a5653e76
YanWQ-monad <YanWQmonad@gmail.com>
2024-05-26 21:13:16 +0800

    test: add test for metadata

--- tests/tests/misc.rs
+++ tests/tests/misc.rs
@@ -1,4 +1,8 @@
+use actix_web::http::StatusCode;
+use actix_web::test;
 use serde::Deserialize;
+
+use crate::common::{create_app, TestApp};
 
 #[derive(Debug, Deserialize)]
 #[serde(deny_unknown_fields)]
@@ -15,4 +19,18 @@ pub struct Page {
 pub struct Place {
     pub id: i32,
     pub name: String,
+}
+
+#[actix_web::test]
+async fn test_list_places() {
+    let app = create_app().await;
+
+    let req = test::TestRequest::get()
+        .uri(&format!("/api/metadata/places"))
+        .to_request();
+    let resp = app.call(req).await.unwrap();
+    assert_eq!(resp.status(), StatusCode::OK);
+
+    let events: Vec<Place> = test::read_body_json(resp).await;
+    assert!(!events.is_empty());
 }
